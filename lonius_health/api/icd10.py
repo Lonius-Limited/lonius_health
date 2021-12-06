6fe6f423fcee037e65df168c63dc47c109d1f374
Amolo <railamolo@gmail.com>
2021-12-06 10:00:55 +0300

    Test pyc delete

--- lonius_health/api/icd10.py
+++ lonius_health/api/icd10.py
@@ -4,12 +4,12 @@
 
 # MEDICAL_STANDARD ="ICD10"
 # DOCTYPE = "Medical Code"
-# def upload():  
+# def upload():   
 #     codes = list(ALL_CODES.keys())
 #     list(map(lambda x: upload_code(x),codes))
 # def upload_code(code):
 #     if frappe.get_value(DOCTYPE, dict(code=code)): return
-#     payload = ALL_CODES[code]
+#     payload = ALL_CODES[code] 
 #     description, is_billable= payload[1], payload[0]
 #     args = dict(doctype=DOCTYPE, medical_code_standard=MEDICAL_STANDARD,code=code,description=description,is_billable=is_billable)
 #     print(args)
